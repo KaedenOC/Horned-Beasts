ea66704d189a8810b33de72023df7423f2e13720
KaedenOC <komeacarr2@gmail.com>
2023-04-10 12:59:46 -0700

    header and footer rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './Header';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header title = {'Horned Beasts'} />
+      <Footer author = {'Kaeden'} />
     </div>
   );
 }
