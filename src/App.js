076ecfbaa9829d8b8b484340665d77267ee6e3e6
KaedenOC <komeacarr2@gmail.com>
2023-04-12 17:14:53 -0700

    json file imported in app and beasts passed to main

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Component } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Beasts from './beasts.json';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main beasts={Beasts} />
+        <Footer />
+      </div>
+    );
+
+  }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row';
 // import Image from 'react-bootstrap/Image';
 // import { HeartFill } from 'react-bootstrap-icons';
 import { Component } from 'react';
-import Beasts from '../beasts.json';
+// import Beasts from '../beasts.json';
 import HornedBeast from './HornedBeast';
 
 
@@ -16,12 +16,12 @@ class Main extends Component {
                 <Container>
                     <Row>
                         {
-                            Beasts.map(beast => {
+                            this.props.beasts.map(beast => {
                                 return (
-                                   <Col xs={12} md={4}>
-                                   <HornedBeast title={beast.title} imageUrl={beast.image_url} description={beast.description} />
-                                   
-                                   </Col>
+                                    <Col xs={12} md={4}>
+                                        <HornedBeast title={beast.title} imageUrl={beast.image_url} description={beast.description} />
+
+                                    </Col>
                                 )
                             })
                         }
